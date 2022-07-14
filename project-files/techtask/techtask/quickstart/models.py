ce2e101c0ed5a4821ce5954b931941e962444943
Alexander Dmitriev <alexander.dmitriev2000@yandex.ru>
2022-07-14 19:31:17 +0300

    Создал модель Person

--- project-files/techtask/techtask/quickstart/models.py
+++ project-files/techtask/techtask/quickstart/models.py
@@ -1,4 +1,6 @@
 from django.db import models
 
 class Person(Models.model):
-    name
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    phone_number = models.CharField(max_length = 20)
