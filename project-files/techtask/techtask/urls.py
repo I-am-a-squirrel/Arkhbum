cbdc2eef15cfe9f963861817691cd816764ab9f9
Alexander Dmitriev <alexander.dmitriev2000@yandex.ru>
2022-07-14 20:44:04 +0300

    Работаю с views, urlpatterns

--- project-files/techtask/techtask/urls.py
+++ project-files/techtask/techtask/urls.py
@@ -26,4 +26,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
+    path('employees-list/', ),
+    path('employee-profile/', ),
 ]
