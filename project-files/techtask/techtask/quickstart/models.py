82ff81928cced52bcef49996d150bfbe0fbcfed5
Alexander Dmitriev <alexander.dmitriev2000@yandex.ru>
2022-07-20 12:08:47 +0300

    Пишу функции в models

--- project-files/techtask/techtask/quickstart/models.py
+++ project-files/techtask/techtask/quickstart/models.py
@@ -9,6 +9,9 @@ class Person(Models.model):
     phone_number = models.CharField(max_length = 20)
     persons_list = models.ForeignKey('PersonsList', on_delete = models.CASCADE)
 
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+
     class Meta:
         ordering = ['created']
 
