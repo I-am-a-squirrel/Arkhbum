91a18c15e704f3f8b00370a891060c2199de2676
Alexander Dmitriev <alexander.dmitriev2000@yandex.ru>
2022-07-26 20:23:13 +0300

    Admin может добавлять Persons через UI

--- project-files/techtask/techtask/quickstart/models.py
+++ project-files/techtask/techtask/quickstart/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 class Person(models.Model):
     id = models.BigAutoField(primary_key = True)
-    owner = models.ForeignKey('auth.User', related_name='quickstart', on_delete=models.CASCADE)
-    #highlighted = models.TextField()
     first_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
     phone_number = models.CharField(max_length = 20)
