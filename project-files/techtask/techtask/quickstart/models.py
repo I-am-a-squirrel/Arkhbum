d6359cee11bcb3c0b62d918b8b8d81a370b008c8
Alexander Dmitriev <alexander.dmitriev2000@yandex.ru>
2022-07-26 23:45:32 +0300

    Исправил отображение списка пользователей

--- project-files/techtask/techtask/quickstart/models.py
+++ project-files/techtask/techtask/quickstart/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Person(models.Model):
     id = models.BigAutoField(primary_key = True)
-    first_name = models.CharField(max_length = 50)
-    last_name = models.CharField(max_length = 50)
-    phone_number = models.CharField(max_length = 20)
+    first_name = models.CharField(max_length = 50, null = True)
+    last_name = models.CharField(max_length = 50, null = True)
+    phone_number = models.CharField(max_length = 20, null = True)
     persons_list = models.ForeignKey('PersonsList', on_delete = models.CASCADE)
 
     def __str__(self):
@@ -14,5 +14,8 @@ class Person(models.Model):
 #        ordering = ['created']
 
 class PersonsList(models.Model):
-    pass
-    #id = models.BigAutoField(primary_key = True)
+    id = models.BigAutoField(primary_key = True)
+    list_name = models.CharField(max_length = 50, null = True)
+
+    def __str__(self):
+        return '%s' % (self.list_name)
