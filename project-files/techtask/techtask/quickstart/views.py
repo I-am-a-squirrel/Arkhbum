c26ffe194c8985d8b125a8352087eb4b9fc7613b
Alexander Dmitriev <alexander.dmitriev2000@yandex.ru>
2022-07-28 21:51:33 +0300

    Улучшил импорты

--- project-files/techtask/techtask/quickstart/views.py
+++ project-files/techtask/techtask/quickstart/views.py
@@ -1,45 +1,47 @@
 from django.contrib.auth.models import User, Group
 from django.shortcuts import render
 
-from rest_framework import viewsets, permissions
+from rest_framework.mixins import RetrieveModelMixin
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.renderers import BrowsableAPIRenderer, TemplateHTMLRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
 from techtask.quickstart.models import Person
-from techtask.quickstart.serializers import UserSerializer, GroupSerializer, PersonSerializer, ListPersonsSerializer
+from techtask.quickstart.serializers import UserSerializer, GroupSerializer, PersonSerializer
 from techtask.permissions import IsOwnerOrReadOnly
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 
-class GroupViewSet(viewsets.ModelViewSet):
+class GroupViewSet(ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
 class ListHTMLPersonsView(APIView):
     queryset = Person.objects.all()
     renderer_classes = [TemplateHTMLRenderer]
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def get(self, request, *args, **kwargs):
         self.object = self.queryset
         return Response({'Persons': self.object}, template_name='all-persons-list.html')
 
-class ShowHTMLPersonSet(viewsets.GenericViewSet):
+class ShowHTMLPersonSet(GenericViewSet, RetrieveModelMixin):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
     renderer_classes = [TemplateHTMLRenderer]
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     
